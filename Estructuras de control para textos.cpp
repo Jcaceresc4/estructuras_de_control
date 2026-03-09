6abffcbb37a416deec890dd9628c5b6da7e86c86
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-08 23:10:37 -0600

    se agrego la estructura para textos

---
+++ Estructuras de control para textos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main(){
+	//numero es positivo>0 o negativo<0
+	string pais="";
+	cout<<"ingrese el pais: ";
+	cin>>pais;
+		if(pais=="Guatemala"){
+			cout<<"yo soy de "<<pais<<" mi pais "<<endl;
+		}else{
+			cout<<pais<<endl;
+		}
+	system("pause");
+}
